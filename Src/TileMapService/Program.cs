a603a2bf0ad7cebadeec750b3e1851542f64b2dd
Andrey <andrey-vat@yandex.ru>
2023-09-16 17:50:55 +0300

    Minor improvement of logging system information

--- Src/TileMapService/Program.cs
+++ Src/TileMapService/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace TileMapService
 {
@@ -10,10 +13,19 @@ namespace TileMapService
     {
         public static async Task Main()
         {
-            var host = CreateHostBuilder().Build();
+            var host = Host.CreateDefaultBuilder()
+                    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                    .Build();
 
             // TODO: check and log configuration errors
             // https://stackoverflow.com/questions/56077346/asp-net-core-call-async-init-on-singleton-service
+
+            using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+                .SetMinimumLevel(LogLevel.Trace)
+                .AddConsole());
+
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation($"System info: {Environment.NewLine}{String.Join(Environment.NewLine, GetEnvironmentInfo())}");
 
             if (host.Services.GetService(typeof(ITileSourceFabric)) is not ITileSourceFabric service)
             {
@@ -25,10 +37,16 @@ namespace TileMapService
             host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder()
-        {
-            return Host.CreateDefaultBuilder()
-                    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
-        }
+        private static string[] GetEnvironmentInfo() =>
+            new[]
+            {
+                $"MachineName='{Environment.MachineName}'  User='{Environment.UserDomainName}\\{Environment.UserName}'  CPU={Environment.ProcessorCount}  OS='{Environment.OSVersion}'",
+                $"OS x64={Environment.Is64BitOperatingSystem}  Process x64={Environment.Is64BitProcess}  .NET='{Environment.Version}'  Culture='{Thread.CurrentThread.CurrentCulture.DisplayName}'",
+                $"UtcOffset={TimeZoneInfo.Local.GetUtcOffset(DateTime.Now)}  TZ='{TimeZoneInfo.Local.StandardName}'",
+                $"UtcNow={DateTime.UtcNow}  Uptime={TimeSpan.FromMilliseconds(Environment.TickCount64)}",
+                $"Process [PID={Environment.ProcessId}]='{Process.GetCurrentProcess()?.MainModule?.FileName}'",
+                $"Assembly='{typeof(Program).Assembly.Location}'",
+                $"CurrentDirectory='{Environment.CurrentDirectory}'",
+            };
     }
 }
